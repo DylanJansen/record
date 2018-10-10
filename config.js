11b191a008cf0e0e6b5d62b1532bbbd7ccadab65
DylanJansen <40144787+DylanJansen@users.noreply.github.com>
2018-10-10 18:43:29 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+/*
+  Bot config vars
+ */
+
+module.exports = {
+  // Bot user token, found on Discord Devs website:
+  token: 'NDk5NjE4NTgzNjUyMDczNDcy.Dp-5wg.1cDImo7-eOrXe5h0MkG120Vo3xE',
+  // The bot app's client ID:
+  clientId: '499618583652073472',
+  // Commands should be preceded by this string:
+  prefix: '@mention',
+  // Moment.js date format to use:
+  dateFormat: 'YY-MM-DD HH:mm',
+};
